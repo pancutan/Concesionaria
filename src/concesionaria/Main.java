34819d9b9dcf948c3330e5e2758d90cc5336152b
Sergio Alonso <sergio@eim.esc.edu.ar>
2010-05-29 00:02:36 -0300

    Clases listas

--- src/concesionaria/Main.java
+++ src/concesionaria/Main.java
@@ -4,7 +4,7 @@ package concesionaria;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 
 }
---
+++ src/concesionaria/Transporte.java
@@ -0,0 +1,12 @@
+package concesionaria;
+
+public class Transporte {
+
+  String marca;
+  String color;
+  int anioFabricac;
+  String tipoCombustible;
+  double precFabrica;
+  int cantidadEnStock;
+  
+}
